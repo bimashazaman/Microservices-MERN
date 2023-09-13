79acb34e96d82274f90859ffbad7eed782de4976
bimashazaman <bimashazaman1234.com>
2023-09-13 14:34:41 +0600

    Initial commit

---
+++ posts/index.js
@@ -0,0 +1,30 @@
+const express = require('express')
+const { randomBytes } = require('crypto')
+
+const bodyParser = require('body-parser')
+
+const app = express()
+app.use(bodyParser.json())
+
+const posts = {}
+
+app.get('/posts', (req, res) => {
+  res.send(posts)
+})
+
+app.post('/posts', (req, res) => {
+  /* The code you provided is handling a POST request to create a new post.  */
+  const id = randomBytes(4).toString('hex')
+  const { title } = req.body
+
+  posts[id] = {
+    id,
+    title,
+  }
+
+  res.status(201).send(posts[id])
+})
+
+app.listen(4000, () => {
+  console.log('Listening on 4000')
+})
