5b43c7a2ce8395606a618a36eb98af388cb69fcd
bimashazaman <bimashazaman1234.com>
2023-10-06 05:39:18 +0600

    Initial commit

---
+++ comments/index.js
@@ -0,0 +1,31 @@
+const express = require('express')
+const { randomBytes } = require('crypto')
+
+const bodyParser = require('body-parser')
+
+const app = express()
+
+app.use(bodyParser.json())
+
+const commentsByPostId = {}
+
+app.get('/posts/:id/comments', (req, res) => {
+  res.send(commentsByPostId[req.params.id] || [])
+})
+
+app.post('/posts/:id/comments', (req, res) => {
+  const commentId = randomBytes(4).toString('hex')
+  const { content } = req.body
+
+  const comments = commentsByPostId[req.params.id] || []
+
+  comments.push({ id: commentId, content })
+
+  commentsByPostId[req.params.id] = comments
+
+  res.status(201).send(comments)
+})
+
+app.listen(4001, () => {
+  console.log('Listening on 4001')
+})
